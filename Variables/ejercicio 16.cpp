4340834ec70100913867d2034e7572769a12bdbe
Matias Fredes <matiasfredes90@gmail.com>
2021-12-20 20:09:45 -0300

    ejercicio 16

---
+++ Variables/ejercicio 16.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	float nota1 , nota2 , nota3 , promedio ;
+	
+	cout<<"ingrese nota 1"<<endl;
+	scanf ("%f",&nota1 );
+	cout<<"ingrese nota 2"<<endl;
+	scanf ("%f",&nota2 );
+	cout<<"ingrese nota 3"<<endl;
+	scanf ("%f",&nota3 );
+	
+	promedio = (nota1 + nota2 + nota3)/3;
+	cout<<"El promedio es "<<promedio<<endl;
+	
+	
+	
+	
+	
+	
+	return 0 ;
+}
+
+
+	
+	
+
